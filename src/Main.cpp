53d4f93d82f02326b8d10024e3258b997e426218
kinorox <gabriel.bombardi@gmail.com>
2017-04-01 20:36:07 -0300

    adicionados dados informativos

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,11 +1,16 @@
 //--------------------------------------------------------------
-// NOMES DOS RESPONSÁVEIS: blablabla E blablabla
+// NOMES DOS RESPONSÁVEIS: Gabriel Bombardi E Marcos Pinheiro Moura
 //--------------------------------------------------------------
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
 #include <malloc.h>
+#include <stddef.h>
+#include <string.h>
+#include <time.h>
+#include <wchar.h>
+#define null 0
 
 // ######### ESCREVA O NROUSP DO PRIMEIRO INTEGRANTE AQUI
 char* nroUSP1() {
@@ -16,7 +21,6 @@ char* nroUSP1() {
 char* nroUSP2() {
     return("7971841");
 }
-
 
 // elemento das listas de adjacência e de resposta
 typedef struct estr {
@@ -55,11 +59,6 @@ NO *caminho(int N, int *ijpeso, int *aberto, int inicio, int fim, int chave)
 //---------------------------------------------------------
 int main() {
 
-
-	// aqui vc pode incluir codigo de teste
-
-	// exemplo de teste trivial
-
 	int N=3; // grafo de 3 vértices 1..3
 	int aberto[] = {1,1,1}; // todos abertos
 	int inicio=1;
@@ -70,9 +69,8 @@ int main() {
 	// o EP sera testado com uma serie de chamadas como esta
 	NO* teste = null;
 	teste = caminho(N, ijpeso, aberto, inicio, fim, chave);
-	return teste;
+	return 1;
 
 }
 
 // por favor nao inclua nenhum código abaixo da função main()
-
