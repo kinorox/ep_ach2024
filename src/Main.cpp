4e39e507fceb4f3cacfa0c9065c8934404638766
kinorox <gabriel.bombardi@gmail.com>
2017-04-01 23:13:29 -0300

    estrutura de filas e de busca em largura

--- src/Main.cpp
+++ src/Main.cpp
@@ -24,18 +24,134 @@ char* nroUSP2() {
 
 // elemento das listas de adjacência e de resposta
 typedef struct estr {
-        int v; // elemento
-        int peso; // custo (não precisa ser usado na resposta)
-        struct estr *prox;
+    int v; // elemento
+    int peso; // custo (não precisa ser usado na resposta)
+    struct estr *prox;
 } NO;
 
 // vertices do grafo (salas) - use este tipo ao criar o grafo
 typedef struct
 {
-       int flag; // para uso na busca em largura e profundidade, se necessario
-       bool aberto; // vale true se a sala em questao esta aberta
-       NO* inicio;
+    int flag; // para uso na busca em largura e profundidade, se necessario
+    bool aberto; // vale true se a sala em questao esta aberta
+    NO* inicio;
 } VERTICE;
+
+typedef struct {
+    NO* inicio;
+    NO* fim;
+} fila;
+
+void entrarFila(fila* f, int ch) {
+
+    NO * novo;
+    novo = (NO*) malloc(sizeof(NO));
+    novo->v = ch;
+    novo->prox = null;
+    if(f->fim) f->fim->prox = novo;
+    else f->inicio = novo;
+    f->fim = novo;
+
+}
+
+int sairFila(fila* f) {
+
+    NO * aux;
+    int ch;
+    if(!f->inicio) return(-1);
+    ch = f->inicio->v;
+    aux = f->inicio;
+    f->inicio = f->inicio->prox;
+    free(aux);
+    if (!f->inicio) f->fim = null;
+
+    return(ch);
+
+}
+
+void inicializar(fila* f) {
+
+    f->inicio = null;
+    f->fim = null;
+
+}
+
+void zerarFlags(VERTICE* g) {
+
+    NO *p = g[0].inicio;
+
+    while(p) {
+        p = p->prox;
+    }
+
+}
+
+void largura(VERTICE* g, int i) {
+
+    zerarFlags(g);
+
+    fila f;
+    inicializar(&f);
+
+    g[i].flag = 1;
+    entrarFila(&f, i);
+
+    while(f.inicio) {
+
+        i = sairFila(&f);
+        NO * p = g[i].inicio;
+        while (p){
+
+            if (g[p->v].flag == 0) {
+                entrarFila (&f, p->v);
+                g[p->v].flag = 1;
+            }
+
+            p = p->prox;
+
+        }
+
+        g[i].flag = 2;
+    }
+
+}
+
+bool arestaExiste (VERTICE* g, int i, int j) {
+    NO *p = g[i].inicio;
+
+    while(p) {
+        if(p->v ==j) return true;
+
+        p = p->prox;
+    }
+
+    return false;
+}
+
+void inserirAresta(VERTICE* g, int i, int j, int peso) {
+
+    if(arestaExiste(g,i,j)) return;
+
+    NO *novo = (NO*) malloc(sizeof(NO));
+
+    novo->v = j;
+    novo->prox = g[i].inicio;
+    novo->peso = peso;
+    g[i].inicio = novo;
+
+}
+
+VERTICE* criaGrafo(int nVertices) {
+
+    VERTICE* g = (VERTICE*) malloc(sizeof(VERTICE) * (nVertices + 1));
+
+    for(int i = 1; i<= nVertices; i++) {
+        g[i].inicio = null;
+    }
+
+    return g;
+
+}
 
 //------------------------------------------
 // O EP consiste em implementar esta funcao
@@ -48,6 +164,12 @@ NO *caminho(int N, int *ijpeso, int *aberto, int inicio, int fim, int chave)
 	resp = NULL;
 
 	// seu codigo AQUI
+
+	VERTICE* g = criaGrafo(N);
+
+	for(int i = 0; i<sizeof(ijpeso); i + 3) {
+        inserirAresta(g, ijpeso[i], ijpeso[i + 1], ijpeso[i + 2]);
+	}
 
 	//...
 
@@ -72,5 +194,4 @@ int main() {
 	return 1;
 
 }
-
 // por favor nao inclua nenhum código abaixo da função main()
